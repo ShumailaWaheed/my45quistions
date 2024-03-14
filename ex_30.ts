2e6e72c200df5ab5720ee08cabfbace4872aa3a4
ShumailaWaheed <162050091+ShumailaWaheed@users.noreply.github.com>
2024-03-15 01:01:45 +0500

    Update ex_30.ts

--- ex_30.ts
+++ ex_30.ts
@@ -1,7 +1,7 @@
 let usernames: string[] = ["admin", "Urooj", "Mehak", "Sidra", "Faryal"];
 
 for (let username of usernames) {
-    if (username ="admin") {
+    if (username ==="admin") {
         console.log("Hello admin, would you like to see a status report?");
     } else {
         console.log(`Hello ${username}, "thank you for logging in again."`);
