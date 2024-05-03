1b1a9d407a45872e32e3a504a93f79283923c5d7
Shumaila Waheed <162050091+ShumailaWaheed@users.noreply.github.com>
2024-05-03 14:51:14 +0500

    Add files via upload

--- ex_40.js
+++ ex_40.js
@@ -1,18 +1,10 @@
-function make_album(artist, title, tracks) {
-    var album = {
-        artist: artist,
-        title: title
-    };
+function makeAlbum(artist, title, tracks) {
+    var album = { artist: artist, title: title };
     if (tracks) {
         album.tracks = tracks;
     }
     return album;
 }
-// Creating album objects
-var album1 = make_album("Nusrat Fateh Ali Khan", "The Legend", 10);
-var album2 = make_album("Junoon", "Azadi");
-var album3 = make_album("Atif Aslam", "Doorie", 12);
-// Printing album information
-console.log(album1);
-console.log(album2);
-console.log(album3);
+console.log(makeAlbum("Atif Aslam", "Jal Pari"));
+console.log(makeAlbum("Noori", "Sunno"));
+console.log(makeAlbum("Strings", "Dhaani", 10));
--- ex_40.ts
+++ ex_40.ts
@@ -1,20 +1,11 @@
-function make_album(artist: string, title: string, tracks?: number): { artist: string, title: string, tracks?: number } {
-    const album: { artist: string, title: string, tracks?: number } = {
-        artist: artist,
-        title: title
-    };
+function makeAlbum(artist: string, title: string, tracks?: number) {
+    let album: { artist: string, title: string, tracks?: number } = { artist, title };
     if (tracks) {
         album.tracks = tracks;
     }
     return album;
 }
 
-// Creating album objects
-const album1 = make_album("Nusrat Fateh Ali Khan", "The Legend", 10);
-const album2 = make_album("Junoon", "Azadi");
-const album3 = make_album("Atif Aslam", "Doorie", 12);
-
-// Printing album information
-console.log(album1);
-console.log(album2);
-console.log(album3);
+console.log(makeAlbum("Atif Aslam", "Jal Pari"));
+console.log(makeAlbum("Noori", "Sunno"));
+console.log(makeAlbum("Strings", "Dhaani", 10));
