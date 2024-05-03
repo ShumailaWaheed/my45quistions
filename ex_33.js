cdeca4c130d9fb555d5631be060d0870bba48745
Shumaila Waheed <162050091+ShumailaWaheed@users.noreply.github.com>
2024-05-03 21:57:59 +0500

    Add files via upload

--- ex_33.js
+++ ex_33.js
@@ -1,32 +1,15 @@
-// Tests for equality and inequality with strings
-var string1 = 'hello';
-var string2 = 'world';
-console.log("Is string1 equal to 'hello'? I predict True.");
-console.log(string1 == 'hello');
-console.log("Is string1 not equal to string2? I predict True.");
-console.log(string1 != string2);
-// Tests using the lower case function
-var str = 'Hello';
-console.log("Is str lowercase 'hello'? I predict False.");
-console.log(str.toLowerCase() == 'hello');
-// Numerical tests
-var num1 = 10;
-var num2 = 5;
-console.log("Is num1 greater than num2? I predict True.");
-console.log(num1 > num2);
-console.log("Is num1 less than or equal to num2? I predict False.");
-console.log(num1 <= num2);
-// Tests using "and" and "or" operators
-var x = 5;
-var y = 10;
-console.log("Is x greater than 3 and y less than 15? I predict True.");
-console.log(x > 3 && y < 15);
-console.log("Is x less than 3 or y greater than 15? I predict False.");
-console.log(x < 3 || y > 15);
-// Test whether an item is in an array
-var numbers = [1, 2, 3, 4, 5];
-console.log("Is 3 in the numbers array? I predict True.");
-console.log(numbers, (3));
-// Test whether an item is not in an array
-console.log("Is 6 not in the numbers array? I predict True.");
-console.log(!numbers, (6));
+"use strict";
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach(number => {
+    let suffix = "th";
+    if (number === 1) {
+        suffix = "st";
+    }
+    else if (number === 2) {
+        suffix = "nd";
+    }
+    else if (number === 3) {
+        suffix = "rd";
+    }
+    console.log(`${number}${suffix}`);
+});
--- ex_33.ts
+++ ex_33.ts
@@ -1,40 +1,13 @@
-// Tests for equality and inequality with strings
-let string1 = 'hello';
-let string2 = 'world';
-console.log("Is string1 equal to 'hello'? I predict True.");
-console.log(string1 == 'hello');
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log("Is string1 not equal to string2? I predict True.");
-console.log(string1 != string2);
-
-// Tests using the lower case function
-let str = 'Hello';
-console.log("Is str lowercase 'hello'? I predict False.");
-console.log(str.toLowerCase() == 'hello');
-
-// Numerical tests
-let num1 = 10;
-let num2 = 5;
-console.log("Is num1 greater than num2? I predict True.");
-console.log(num1 > num2);
-
-console.log("Is num1 less than or equal to num2? I predict False.");
-console.log(num1 <= num2);
-
-// Tests using "and" and "or" operators
-let x = 5;
-let y = 10;
-console.log("Is x greater than 3 and y less than 15? I predict True.");
-console.log(x > 3 && y < 15);
-
-console.log("Is x less than 3 or y greater than 15? I predict False.");
-console.log(x < 3 || y > 15);
-
-// Test whether an item is in an array
-let numbers = [1, 2, 3, 4, 5];
-console.log("Is 3 in the numbers array? I predict True.");
-console.log(numbers,(3));
-
-// Test whether an item is not in an array
-console.log("Is 6 not in the numbers array? I predict True.");
-console.log(!numbers,(6));
+numbers.forEach(number => {
+    let suffix = "th";
+    if (number === 1) {
+        suffix = "st";
+    } else if (number === 2) {
+        suffix = "nd";
+    } else if (number === 3) {
+        suffix = "rd";
+    }
+    console.log(`${number}${suffix}`);
+});
