e97f338d5b1835e63fd0b94aea47aa809865ff83
Shumaila Waheed <162050091+ShumailaWaheed@users.noreply.github.com>
2024-05-03 22:48:19 +0500

    Add files via upload

--- ex_36.js
+++ ex_36.js
@@ -1,5 +1,7 @@
+"use strict";
 function make_shirt(size, message) {
-    console.log("The shirt size is ".concat(size, " and it has the following message: \"").concat(message, "\"."));
+    console.log(`The shirt size is ${size} 
+    and it has the following message: "${message}".`);
 }
 // Call the function
-make_shirt("L", "I love TypeScript!");
+make_shirt("Medium", "I love My Father!");
--- ex_36.ts
+++ ex_36.ts
@@ -1,6 +1,7 @@
-function make_shirt(size: string, message: string): void {
-    console.log(`The shirt size is ${size} and it has the following message: "${message}".`);
+function make_shirt(size: string, message: string) {
+    console.log(`The shirt size is ${size} 
+    and it has the following message: "${message}".`);
 }
 
 // Call the function
-make_shirt("L", "I love TypeScript!");
+make_shirt("Medium", "I love My Father!");
